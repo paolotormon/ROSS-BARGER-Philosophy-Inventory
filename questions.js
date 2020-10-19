2e071d3b3c333af2368ae8369021b165ebb887f5
Paolo Tormon <mdawntormon@gmail.com>
2020-10-19 22:49:19 +0800

    add questions.js

--- questions.js
+++
@@ -1,4 +0,0 @@
-var arr = [
-  "Learning is a PROCESS OF INTERACTING with people and things around us. It leads to new understandings which can then be used to solve social  problems.",
-  "The human person is PRIMARILY A NERVOUS SYSTEM which is influenced by interaction with the physical environment along lines recognized by science.",
-]
